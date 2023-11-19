daf94a170e2c8a2519909a2f30c99267904eca67
gbc8 <gbc7@cin.ufpe.br>
2023-11-19 16:25:13 -0300

    Simple Message

---
+++ Tutorials/SimpleMessage/Send.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	connection, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	failOnError(err, "Fail to connect to RabbitMQ")
+
+	defer connection.Close()
+
+	ch, err := connection.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	queue, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var body string
+
+	fmt.Println("Write the message to send:")
+	fmt.Scan(&body)
+
+	err = ch.PublishWithContext(ctx,
+		"",         // exchange
+		queue.Name, // routing key
+		false,      // mandatory
+		false,      // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	log.Printf(" [x] Sent %s\n", body)
+}
